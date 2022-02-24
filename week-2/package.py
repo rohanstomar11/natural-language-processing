392f55ff3a39381f266259e68c83474a08b1ff91
Rohan Singh <rohanstomar11@gmail.com>
2022-02-25 02:41:13 +0530

    rearranges files

---
+++ week-2/package.py
@@ -0,0 +1,51 @@
+import nltk
+import re
+import numpy as np
+import string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+
+
+def process_tweets(tweet):
+    stemmer = PorterStemmer()
+    stopwords_english = stopwords.words('english')
+    tweet = re.sub(r'\$\w*', '', tweet)
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    tweet = re.sub(r'https?://[^\s\n\r]+', '', tweet)
+    tweet = re.sub(r'#', '', tweet)
+
+    tokenizer = TweetTokenizer(
+        preserve_case=False, strip_handles=True, reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (word not in stopwords_english and word not in string.punctuation):
+            stem_word = stemmer.stem(word)
+            tweets_clean.append(stem_word)
+
+    return tweets_clean
+
+
+def build_freqs(tweets, ys):
+    yslist = np.squeeze(ys).tolist()
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweets(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1
+    return freqs
+
+
+def lookup(freqs, word, label):
+    n = 0
+
+    pair = (word, label)
+    if (pair in freqs):
+        n = freqs[pair]
+
+    return n
