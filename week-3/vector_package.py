a508ccdeed470d10c805cee2c8a64264cdd3f46c
Rohan Singh <rohanstomar11@gmail.com>
2022-02-25 14:40:12 +0530

    added country predictor

---
+++ week-3/vector_package.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def get_vecs(embeddings, words):
+    m = len(words)
+    X = np.zeros((1,300))
+    for word in words:
+        x = word
+        embedd = embeddings[x]
+        X = np.row_stack((X, embedd))
+    #X=X[1:,:]    
+    return X
